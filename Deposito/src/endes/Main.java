4fa1a6ccb1a92efdcda7ba5f3c23c1a6d576cadc
Mariano <mariano.a.messana@gmail.com>
2021-03-20 11:40:08 +0100

    Cambio mensaje

--- Deposito/src/endes/Main.java
+++ Deposito/src/endes/Main.java
@@ -3,6 +3,8 @@ package endes;
 public class Main {
 
     public static void main(String[] args) {
+    	System.out.println("Deposito"); 
+    	
         CCuenta miCuenta;
         double saldoActual;
 
